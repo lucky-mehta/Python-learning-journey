ec436d1abcda46b15fc9a548b1f39ec976b999f3
lucky-mehta <mehtalucky2005@gmail.com>
2026-09-09 07:31:52 +0530

    string commit

---
+++ String/string.py
@@ -0,0 +1,8 @@
+name="lucky"
+print("My name is:",name)
+print("Hello,"+name)
+print(name[0])     #indexing
+print(name[1])  
+print(name[2])  
+print(name[3])  
+print(name[4])  
---
+++ String/string1.py
@@ -0,0 +1,9 @@
+apple='''He said,
+Hii Lucky
+hey I am good
+"I want to eat apple"
+'''
+print(apple)
+print("Lets use a for loop in string\n")
+for charecter in apple:
+  print(charecter)
